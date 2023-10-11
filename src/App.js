930d8ba8a2efe6daafa45dfa329c5ba6c194763e
kelocoes <kevin.rodriguez109@gmail.com>
2023-10-10 19:37:51 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CameraCanvas from './Components/Webcam/CameraCanvas'
+
+function App() {
+  return (
+    <div className="App">
+      <CameraCanvas></CameraCanvas>
+    </div>
+  );
+}
+
+export default App;
