c3479d6b58781fa16f80ecdeaf98c3a138302bab
kelocoes <kevin.rodriguez109@gmail.com>
2023-12-18 12:43:21 -0500

    Finishing

--- src/Api/ApiUsers.js
+++ src/Api/ApiUsers.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const useRequest = () => {
 
-    const mockUrl = "https://practicainvestigativauv";
+    const mockUrl = "http://localhost:5000";
 
     const makeRequest = async (options) => {
         try {
